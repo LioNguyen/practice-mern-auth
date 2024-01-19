4f2bf8dcd770a08db1090c5ed9dbfa995c0c2c6b
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-19 17:43:47 +0700

    feat: create model, controllers, routes for backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/useRoutes.js";
+
+dotenv.config();
+const port = process.env.PORT || 5001;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Setup backend
+
+```json
+{
+  "name": "practice-mern-auth",
+  "version": "1.0.0",
+  "type": "module", // This will enable import export method
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node backend/server.js",
+    "server": "nodemon backend/server.js"
+  }
+}
+```
