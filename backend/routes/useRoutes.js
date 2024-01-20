c2668297423f4509710f561e6ae3a7bc34594887
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-20 20:39:39 +0700

    feat: add middleware to handle cookie

--- backend/routes/useRoutes.js
+++ backend/routes/useRoutes.js
@@ -6,12 +6,16 @@ import {
   registerUser,
   updateUserProfile,
 } from "../controllers/useController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -14,6 +15,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
